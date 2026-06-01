1b86b1b87985d10b1d2fd93a2b98d2822972ae01
dhvanil_145 <dhvanilgaudani00@gmail.com>
2026-06-01 15:14:48 +0530

    ui is created

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -36,58 +36,58 @@ const ExpenseForm = ({ onSubmit,editData }) => {
   };
 
   return (
-    <div>
-      <form action="" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="amount">Enter amount</label>
-
-          <input
-            type="number"
-            name="amount"
-            id="amount"
-            placeholder="Enter the amount"
-            required
-            onChange={handleChange}
-            value={formData.amount}
-          />
+    <div className="max-w-3xl mx-auto flex flex-col gap-3 mt-8">
+    <h1 className="font-black text-3xl">Expense Tracker app</h1>
+      <form action="" onSubmit={handleSubmit} className="border rounded-lg bg-taupe-50 flex flex-col p-4 gap-3">
+        <div className="flex">
+          <div>
+            <label htmlFor="amount" >Enter amount</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              placeholder="Enter the amount"
+              required
+              onChange={handleChange}
+              value={formData.amount}
+              className="bg-white border border-gray-300 rounded-md px-2.5 py-1.5"
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Enter the description</label>
+            <input
+              type="text"
+              name="description"
+              id="description"
+              placeholder="Enter the description"
+              required
+              onChange={handleChange}
+              value={formData.description}
+              className="bg-white border border-gray-300 rounded-md px-2.5 py-1.5"
+            />
+          </div>
+          <div>
+            <label htmlFor="catagory">
+              Select Catagory of expense:
+            </label>
+            <select
+              name="catagory"
+              id="catagory"
+              onChange={handleChange}
+              value={formData.catagory}
+              className="bg-white border border-gray-300 rounded-md px-2.5 py-1.5"
+            >
+              <option value="" disabled hidden>Select catagory</option>
+              <option value="Food">Food</option>
+              <option value="Travel">Travel</option>
+              <option value="grocery">grocery</option>
+              <option value="Electric">Electric</option>
+            </select>
+          </div>
+          
         </div>
-
-        <div>
-          <label htmlFor="description">Enter the description</label>
-
-          <input
-            type="text"
-            name="description"
-            id="description"
-            placeholder="Enter the description"
-            required
-            onChange={handleChange}
-            value={formData.description}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="catagory">
-            Select Catagory of expense:
-          </label>
-
-          <select
-            name="catagory"
-            id="catagory"
-            onChange={handleChange}
-            value={formData.catagory}
-          >
-            <option value="" disabled hidden></option>
-
-            <option value="Food">Food</option>
-            <option value="Travel">Travel</option>
-            <option value="grocery">grocery</option>
-            <option value="Electric">Electric</option>
-          </select>
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit" className="bg-indigo-600 text-white self-start px-3 py-1 rounded-lg cursor-pointer">Submit</button>
+        </form>
     </div>
   );
 };
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -2,25 +2,30 @@ import React from "react";
 
 const ExpenseList = ({ data, handleDelete, total, handleFilter,handleEdit }) => {
   return (
-    <div>
-      <h1>Total:{total}</h1>
-      <button onClick={() => handleFilter("All")}>All</button>
-      <button onClick={() => handleFilter("Food")}>Food</button>
-      <button onClick={() => handleFilter("Travel")}>Travel</button>
-      <button onClick={() => handleFilter("grocery")}>Grocery</button>
-      <button onClick={() => handleFilter("Electric")}>Electric</button>
+    <div className="max-w-3xl mx-auto flex flex-col gap-4">
+      <h2 className="text-2xl text-indigo-600 bg-purple-100 font-bold rounded-sm border border-gray-300 mt-4">Total: {total}</h2>
+      <div className="flex gap-5 *:bg-indigo-600 *:text-white *:rounded-lg *:cursor-pointer *:px-3 *:py-1.5">
+        <button onClick={() => handleFilter("All")} >All</button>
+        <button onClick={() => handleFilter("Food")} >Food</button>
+        <button onClick={() => handleFilter("Travel")} >Travel</button>
+        <button onClick={() => handleFilter("grocery")} >Grocery</button>
+        <button onClick={() => handleFilter("Electric")} >Electric</button>
+      </div>
       {data.map((cdata) => {
         console.log(cdata);
         return (
           <React.Fragment key={cdata.id}>
-            <div>Amount:{cdata.amount}</div>
-
-            <div>Description:{cdata.description}</div>
-
-            <div>Catagory:{cdata.catagory}</div>
-
-            <button onClick={() => handleDelete(cdata.id)}>Delete</button>
-            <button onClick={() => handleEdit(cdata.id)}>Edit</button>
+            <div className="border border-gray-300 rounded-lg flex justify-between items-center bg-taupe-50 p-4">
+              <div>
+                <div><strong>Description:</strong> {cdata.description}</div>
+                <div><strong>Catagory:</strong> {cdata.catagory}</div>
+              </div>
+              <div>
+                <div className="text-green-500 font-bold ">💲:{cdata.amount}</div>
+                <button onClick={() => handleDelete(cdata.id)} className=" bg-indigo-600 text-white rounded-lg cursor-pointer px-1.5 py-1.5">Delete</button>
+                <button onClick={() => handleEdit(cdata.id)} className=" bg-indigo-600 text-white rounded-lg cursor-pointer px-2.5 py-1.5 ms-2">Edit</button>
+              </div>
+            </div>
           </React.Fragment>
         );
       })}
